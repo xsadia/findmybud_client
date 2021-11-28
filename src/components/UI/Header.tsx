d563faeaed0ea26827d68f9cbe8a0c97100a219e
xsadia <lipearthur@hotmail.com>
2021-11-28 19:31:35 -0300

    feat(init)

---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaPaw } from 'react-icons/fa';
+
+export const Header = () => {
+    return (
+        <header className="flex items-center h-12 bg-blue-500 px-4" >
+            <h1 className="text-gray-50" >Oi</h1>
+        </header>
+    );
+};
